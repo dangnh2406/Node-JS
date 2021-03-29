838d776b5aa07434f7d9e89b8c54a77925e53ca0
dangnh2406 <dangnh2406@gmail.com>
2021-03-29 07:47:27 +0700

    Update

--- assets/js/network.js
+++ assets/js/network.js
@@ -1,39 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var path = require('path');
 
-var http = require('http');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', (process.env.PORT || 5000));
 
-const fs = require('fs');
+app.set('views', __dirname + '/src/views/login.html');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 
-http.createServer(function (require, reponse) {
-    reponse.writeHead('200', { 'Content-type': 'text/html' });
-    var url = require.url;
+app.get('/', function(req, res){
+    res.render('../../src/views/login.html');
+});
 
-    if (url == '/') {
-        //truy cap trang chu
-        fs.readFile('../../src/views/login.html','utf-8', function (errol, data) {
-            if (!errol) {
-                reponse.write(data);
-                console.log(data)
-                reponse.end();
-            }
-            else {
-                console.log(url)
-                reponse.end("404 not found");
-
-            }
-        })
-    }
-    else if (url == '/insert') {
-
-        ///truy cap trang login
-    }
-    else if (url == '/show') {
-
-        ///truy cap trang login
-    }
-    else if (url == '/delete') {
-
-        ///truy cap trang login
-    }
-
-}).listen(process.env.PORT || '3000');
+app.listen(app.get('port'), function() {
+});
