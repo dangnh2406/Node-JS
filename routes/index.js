66b5a625e82fd596cacecd247e528552b93a2241
dangnh2406 <dangnh2406@gmail.com>
2021-04-02 08:58:10 +0700

    Update

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,36 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    console.log('Nguyen Hai Dang1')
+    res.render('login', {title: 'Express'});
 });
+
+
+router.get('/main', function (req, res, next) {
+    console.log('Nguyen Hai Dang2')
+    res.render('main', {title: 'Express'});
+
+});
+
+router.get('/profile.hbs', function (req, res, next) {
+    console.log('Nguyen Hai Dang2')
+    res.render('profile', {title: 'Express'});
+
+});
+
+router.get('/table.hbs', function (req, res, next) {
+    console.log('Nguyen Hai Dang2')
+    res.render('table', {title: 'Express'});
+
+});
+
+router.get('/index.hbs', function (req, res, next) {
+    console.log('Nguyen Hai Dang2')
+    res.render('main', {title: 'Express'});
+
+});
+
+
 
 module.exports = router;
